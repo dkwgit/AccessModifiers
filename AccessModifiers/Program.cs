2caac804c1259df9fdd7797da99152beac663497
David K. Wright <720482+dkwgit@users.noreply.github.com>
2021-12-06 17:41:26 -0600

    Update Program.cs

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -3,7 +3,16 @@ Console.WriteLine("Hello, World!");
 
 internal class Greeter : IGreeter
 {
+    // <= Generates Severity	Error	CS0737	'Greeter' does not implement interface member 'IGreeter.Greet(string)'.
+    // 'Greeter.Greet(string)' cannot implement an interface member because it is not public.
+    //
+    // Being explicit:
+    //    internal string IGreeter.Greet(string person)
+    //
+    // also does not work.
+    // What works is making it public or giving no access modifier.
     internal string Greet(string person)
+
     {
         return $"Hello, {person}";
     }
