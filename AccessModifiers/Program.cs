a42ce34c8ed263ff6f22a8e8129242aa776057bd
David Wright <720482+dkwgit@users.noreply.github.com>
2021-12-06 17:23:05 -0600

    First commit

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+internal class Greeter : IGreeter
+{
+    internal string Greet(string person)
+    {
+        return $"Hello, {person}";
+    }
+}
+
+internal interface IGreeter
+{
+    internal string Greet(string person);
+}
